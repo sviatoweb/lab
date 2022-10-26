d6eafe4f83bd44e39e31777963c7e9011acb5c8e
Sviatoslav <stehnii.pn@ucu.edu.ua>
2022-10-26 23:49:34 +0300

    improved cvs to txt

--- lab.py
+++ lab.py
@@ -1,16 +1,17 @@
-import csv
-
-
 def read_matrix(file):
     """
     str -> list
     Reads the given file with matrix and return every row in a list
     """
-    with open(file, 'r', encoding='utf-8') as file:
-        csvreader = csv.reader(file)
+    with open(file, 'r', encoding='utf-8') as f:
         rows = []
-        for row in csvreader:
+        line = f.readline()[:-1]
+        for i in range(len(line)):
+            row = []
+            for el in line:
+                row.append(el)
             rows.append(row)
+            line = f.readline()[:-1] 
         return rows
 
 
@@ -20,21 +21,23 @@ def write_matrix(rows, file):
     Writes it in the file.
     """
     with open(file, 'w', newline='', encoding='utf-8') as f:
-        writer = csv.writer(f)
         for row in rows:
-            writer.writerow(row)
+            for el in row:
+                f.write(el)
+            f.write('\n')
+
 
 
 def reflective(matrix_r):
     """
     str -> str
     Makes reflexive closure from given matrix.
-    Writes it in reflective.csv and returns reflective matrix.
+    Writes it in reflective.txt and returns reflective matrix.
     """
     for i in range(len(matrix_r)):
-        if matrix_r[i][i]!=1:
-            matrix_r[i][i]=1
-    write_matrix(matrix_r, 'reflective.csv')
+        if matrix_r[i][i]!='1':
+            matrix_r[i][i]='1'
+    write_matrix(matrix_r, 'reflective.txt')
     return matrix_r
 
 
@@ -42,13 +45,13 @@ def warshall(matrix_t):
     """
     str -> str
     Makes transitive closure from given matrix.
-    Writes it in warshall.cvs and returns transitive matrix.
+    Writes it in warshall.txt and returns transitive matrix.
     """
     for k in range(1,len(matrix_t)):
         for i in range(1,len(matrix_t)):
             for j in range(1,len(matrix_t)):    
                 matrix_t[i][j]=str(int(matrix_t[i][j]) | (int(matrix_t[i][k])& int(matrix_t[k][j])))
-    write_matrix(matrix_t, 'warshall.csv')
+    write_matrix(matrix_t, 'warshall.txt')
     return matrix_t
 
 
@@ -56,13 +59,13 @@ def symetrical(matrix_s):
     """
     str -> str
     Makes symetrical closure from given matrix.
-    Writes it in symetrical.cvs and returns symetrical matrix.
+    Writes it in symetrical.txt and returns symetrical matrix.
     """
     for i in range(len(matrix_s)):
         for y in range(len(matrix_s[0])):
             if matrix_s[i][y]=='1':
                 matrix_s[y][i]='1'
-    write_matrix(matrix_s, 'symetrical.csv')
+    write_matrix(matrix_s, 'symetrical.txt')
     return matrix_s
 
 
